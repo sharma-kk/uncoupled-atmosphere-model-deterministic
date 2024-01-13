2f5b098aa6cb5d0d6e39efad86e61392701aad60
sharma-kk <sharma.uhh@gmail.com>
2024-01-13 17:57:42 +0100

    updated info and added new function in utilities relevant to compatible FE

--- utilities.py
+++ utilities.py
@@ -16,6 +16,22 @@ def solve_for_v_given_q(q):
     solve(a == l, psi_0, bcs = bc)
     return as_vector([-psi_0.dx(1), psi_0.dx(0)])
 
+def comp_solve_for_v_given_q(q):
+    """ using compatible FE spaces, given vorticity, solve for corresp. vel. Vel obtained needs to be projected using project(v,fucntion_space)"""
+    msh = q.function_space().mesh()
+    Vcg = FunctionSpace(msh, "CG", 2)
+
+    psi = TrialFunction(Vcg)
+    phi = TestFunction(Vcg)
+
+    a = (inner(grad(psi), grad(phi))) * dx
+    l = -q * phi * dx
+    psi_0 = Function(Vcg)
+
+    bc = DirichletBC(Vcg, 0.0, 'on_boundary')
+    solve(a == l, psi_0, bcs = bc)
+    return as_vector([-psi_0.dx(1), psi_0.dx(0)])
+
 def q_from_v(u):
     """calculate vorticity from velocity. q = curl(u)"""
     msh = u.function_space().mesh()
